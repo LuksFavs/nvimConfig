bcd5b8e21048716e72a30aeae9cbd602b583696d
LuksFavs <lucasffas@dcc.ufrj.br>
2024-06-26 15:12:43 -0300

    changed indent size to 2 and added remaps to add : and ; to end of line

--- lua/lks/core/options.lua
+++ lua/lks/core/options.lua
@@ -6,10 +6,10 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
+opt.tabstop = 2
+opt.shiftwidth = 2
 --
 
 opt.wrap = false
@@ -28,7 +28,7 @@ opt.signcolumn = "yes"
 --
 
 -- backspace
-opt.backspace = "indent,eol,start" 
+opt.backspace = "indent,eol,start"
 --
 
 -- split window
